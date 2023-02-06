74c62fc5d45a9a79176da454700b6cd9f2945cc5
nhb1998 <849990475@qq.com>
2023-02-07 00:04:33 +0800

    安装并配置typeorm

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,30 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './controllers/app.controller';
 import { AppService } from './providers/app.service';
 import appConfig from './configs/app.config';
+import dbConfig from './configs/db.config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true, load: [appConfig] })],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true, load: [appConfig, dbConfig] }),
+
+    /**
+     * 数据库
+     */
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get('db.host'),
+        port: configService.get('db.port'),
+        username: configService.get('db.username'),
+        password: configService.get('db.password'),
+        database: configService.get('db.database'),
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

63a033a33365e57baf6cedfed32dcf9b1eeb0457
binner0880 <849990475@qq.com>
2023-03-02 10:27:21 +0800

    使用typeorm queryBuilder处理实体数据

--- src/user/show/queris/get-user.query.handler.ts
+++ src/user/show/queris/get-user.query.handler.ts
@@ -1,12 +1,24 @@
 import { IQueryHandler, QueryHandler, EventBus } from '@nestjs/cqrs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from '../../entities/user.entity';
+import { Repository } from 'typeorm';
 import { GetUserQuery } from './get-user.query';
 
 @QueryHandler(GetUserQuery)
 export class GetUserQueryHandler implements IQueryHandler<GetUserQuery> {
-  constructor(private readonly eventBus: EventBus) {}
+  constructor(
+    private readonly eventBus: EventBus,
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
   async execute(query: GetUserQuery) {
     const { id } = query.params;
-    return `query user: ${id}`;
+    // return this.userRepository.findOneBy({ id });
+    return this.userRepository
+      .createQueryBuilder('user')
+      .select(['user.id', 'user.name'])
+      .where('user.id = :userId', { userId: id })
+      .getOne();
   }
 }
--- src/user/show/user-show.module.ts
+++ src/user/show/user-show.module.ts
@@ -1,9 +1,11 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
+import { UserModule } from '../user.module';
 import { UserShowController } from './controllers/user-show.controller';
 import { GetUserQueryHandler } from './queris/get-user.query.handler';
 
 @Module({
   controllers: [UserShowController],
   providers: [GetUserQueryHandler],
+  imports: [forwardRef(() => UserModule)],
 })
 export class UserShowModule {}

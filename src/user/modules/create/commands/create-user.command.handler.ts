ca5b5f3adce4fe74f150d346482fc91cc54ac264
binner0880 <849990475@qq.com>
2023-03-02 11:57:50 +0800

    存储用户数据

--- src/user/modules/create/commands/create-user.command.handler.ts
+++ src/user/modules/create/commands/create-user.command.handler.ts
@@ -1,4 +1,7 @@
 import { CommandHandler, ICommandHandler, EventBus } from '@nestjs/cqrs';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/entities/user.entity';
+import { Repository } from 'typeorm';
 import { UserCreatedEvent } from '../events/user-created.event';
 import { CreateUserCommand } from './create-user.command';
 
@@ -6,11 +9,17 @@ import { CreateUserCommand } from './create-user.command';
 export class CreateUserCommandHandler
   implements ICommandHandler<CreateUserCommand>
 {
-  constructor(private readonly eventBus: EventBus) {}
+  constructor(
+    private readonly eventBus: EventBus,
+    @InjectRepository(UserEntity)
+    private readonly userRepository: Repository<UserEntity>,
+  ) {}
 
   async execute(command: CreateUserCommand) {
-    const { name, password } = command.params;
-    this.eventBus.publish(new UserCreatedEvent({ name, password }));
-    return `create user: ${name} / ${password}`;
+    const user = await this.userRepository.save(command.params);
+    this.eventBus.publish(new UserCreatedEvent(user));
+    return {
+      insertId: user.id,
+    };
   }
 }

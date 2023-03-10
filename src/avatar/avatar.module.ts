7d63bffa85c0bc75223a8fcdbf2ffbe153648a7b
binner0880 <849990475@qq.com>
2023-03-10 15:44:12 +0800

    定义头像实体（AvatarEntity）

--- src/avatar/avatar.module.ts
+++ src/avatar/avatar.module.ts
@@ -1,9 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppUploadModule } from 'src/app/modules/upload/app-upload.module';
 import { AvatarController } from './controllers/avatar.controller';
+import { AvatarEntity } from './entities/avatar.entity';
+import { AvatarEntitySubscriber } from './entities/avatar.entity.subscriber';
 
 @Module({
   controllers: [AvatarController],
-  imports: [AppUploadModule.register({ destination: 'upload.avatar' })],
+  imports: [
+    AppUploadModule.register({ destination: 'upload.avatar' }),
+    TypeOrmModule.forFeature([AvatarEntity]),
+  ],
+  exports: [TypeOrmModule],
+  providers: [AvatarEntitySubscriber],
 })
 export class AvatarModule {}

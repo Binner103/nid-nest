471d3ef1d092f877df17f0c3cc1bd35177a54efe
binner0880 <849990475@qq.com>
2023-03-13 11:18:23 +0800

    定义缩放（调整大小）图像命令（ResizeImageCommand）

--- src/app/modules/image/commands/resize-image.command.ts
+++ src/app/modules/image/commands/resize-image.command.ts
@@ -1,5 +1,17 @@
+import { FitEnum } from 'sharp';
+
+export interface ResizeImageSize {
+  suffix: string;
+  width?: number;
+  height?: number;
+  fit?: keyof FitEnum;
+}
+
 export interface ResizeImageCommandParams {
-  
+  distination?: string;
+  filepath: string;
+  filename: string;
+  sizes: Array<ResizeImageSize>;
 }
 
 export class ResizeImageCommand {

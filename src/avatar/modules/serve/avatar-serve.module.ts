c225ecd45100887c84ffb758b7163c48c400be8d
binner0880 <849990475@qq.com>
2023-03-13 10:19:39 +0800

    导入获取用户头像的查询

--- src/avatar/modules/serve/avatar-serve.module.ts
+++ src/avatar/modules/serve/avatar-serve.module.ts
@@ -2,10 +2,11 @@ import { forwardRef, Module } from '@nestjs/common';
 import { AvatarModule } from 'src/avatar/avatar.module';
 import { AvatarServeController } from './controllers/avatar-serve.controller';
 import { AvatarServeService } from './providers/avatar-serve.service';
+import { GetUserAvatarQueryHandler } from './queries/get-user-avatar.query.handler';
 
 @Module({
   controllers: [AvatarServeController],
-  providers: [AvatarServeService],
+  providers: [AvatarServeService, GetUserAvatarQueryHandler],
   imports: [forwardRef(() => AvatarModule)],
 })
 export class AvatarServeModule {}
